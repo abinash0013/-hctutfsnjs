ffdc03e0c1ff6755b2ce4fe7d41bc2f8becc3e2d
Abinash Sonar <74831561+abinash0013@users.noreply.github.com>
2024-07-15 23:48:06 +0530

    adding incomplete route file

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,2 +1,41 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModal from "@/modal/user";
+import bycript from "bcryptjs";
+
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request){
+    await dbConnect()
+try {
+    const {username, email, password} = await request.json()
+    const existingUserVerifiedByUsername = await UserModal.findOne({
+        username,
+        isVerified: true
+    })
+    if(existingUserVerifiedByUsername){
+        return Response.json({
+            success: false,
+            message: "Username is already taken"
+        }, {status: 400})
+    }
+    const existingUserByEmail = await UserModal.findOne({email})
+    if(existingUserByEmail){
+
+    } else {
+        const hasedPassword = await bcrypt.hash(password, 10)
+        const expireDate = new Date();
+        expiryDate.setHours(expiryDate.getHours() + 1)
+    }
+} catch (error) {
+    console.error("Error Registering user", error);
+    return Response.json({
+        success: false,
+        message: "Error Registering user"
+    },
+      {
+        status: 500,
+    })
+    
+    
+}
+}
